2294b529dc229aa6c29a101db72b839663b8ad96
andreasohlund <andreas_ohlund@hotmail.com>
2010-08-10 21:46:59 +0200

    Added a sample for send only endpoints

---
+++ SendOnlyEndpoint/Program.cs
@@ -0,0 +1,24 @@
+﻿using NServiceBus;
+
+namespace SendOnlyEndpoint.Custom
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Configure.With()
+                .DefaultBuilder()
+                .XmlSerializer()
+                .MsmqTransport()
+                .UnicastBus()
+                .CreateBus()
+                .Start();
+
+            bus.Send("SendOnlyDestination",new TestMessage());
+        }
+    }
+
+    public class TestMessage : IMessage
+    {
+    }
+}
